ebfd27ca815f3696acc386c0205b4a9bc5ae7b21
JunRock <wnstjr120422@naver.com>
2024-03-13 13:03:21 +0900

    feat : initial commit

---
+++ src/main/java/io/festival/distance/DistanceApplication.java
@@ -0,0 +1,13 @@
+package io.festival.distance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistanceApplication.class, args);
+	}
+
+}
