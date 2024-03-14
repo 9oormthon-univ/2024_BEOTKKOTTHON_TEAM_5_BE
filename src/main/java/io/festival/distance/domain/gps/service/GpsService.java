6207ff791516e6700b5951263a8d6f0fd7c62700
hyeesw <hyeeun7904@kookmin.ac.kr>
2024-03-14 12:48:00 +0900

    feat : gps 갱신 로직 (GpsController, GpsService)

---
+++ src/main/java/io/festival/distance/domain/gps/service/GpsService.java
@@ -0,0 +1,28 @@
+package io.festival.distance.domain.gps.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import io.festival.distance.domain.gps.dto.GpsDto;
+import io.festival.distance.domain.member.entity.Member;
+import io.festival.distance.domain.member.repository.MemberRepository;
+import io.festival.distance.exception.DistanceException;
+import io.festival.distance.exception.ErrorCode;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class GpsService {
+	private final MemberRepository memberRepository;
+
+	/** NOTE
+	 * member 테이블의 longitude, latitude 갱신
+	 */
+	@Transactional
+	public Long updateGps(Long memberId, GpsDto gpsDto) {
+		Member member = memberRepository.findById(memberId)
+				.orElseThrow(() -> new DistanceException(ErrorCode.NOT_EXIST_MEMBER));
+		member.memberGpsUpdate(gpsDto);
+		return memberId;
+	}
+}
