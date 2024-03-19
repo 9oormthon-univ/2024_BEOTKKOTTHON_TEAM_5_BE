f5c3b684091460ab65898bf4dd45c4fad1fe62ea
hyeesw <hyeeun7904@kookmin.ac.kr>
2024-03-20 02:51:51 +0900

    refactor : 주석 달기

--- src/main/java/io/festival/distance/domain/firebase/config/FirebaseConfig.java
+++ src/main/java/io/festival/distance/domain/firebase/config/FirebaseConfig.java
@@ -25,17 +25,18 @@ public class FirebaseConfig {
 	/** NOTE
 	 * GoogleCredentials 객체를 생성 (Firebase에 접근시 서버 인증 정보)
  	 */
+	// Firebase Admin SDK private key 파일 경로
 	static final String FILEPATH = "src/main/java/io/festival/distance/domain/firebase/config/firebaseAdminSDKPrivateKey.json";
-	///Users/hyeesw/4-1/distance/BE/src/main/java/io/festival/distance/domain/firebase/config/firebaseAdminSDKPrivateKey.json
 	@PostConstruct
 	public void initialize() {
 		try {
 			String fullPath = System.getProperty("user.dir") + "/" + FILEPATH;
 			System.out.println(fullPath);
+			// FirebaseOptions 객체를 생성 (Firebase에 접근시 서버 인증 정보)
 			FirebaseOptions options = FirebaseOptions.builder()
 				.setCredentials(GoogleCredentials.fromStream(new FileInputStream(fullPath)))
 				.build();
-			// 초기화에 실패했다면 option 인자에 따라 다시 초기화
+			// FirebaseApp 객체가 비어있다면 option 인자에 따라 다시 초기화
 			if (FirebaseApp.getApps().isEmpty()) {
 				System.out.println("FirebaseApp 비어있어요");
 				FirebaseApp.initializeApp(options);
