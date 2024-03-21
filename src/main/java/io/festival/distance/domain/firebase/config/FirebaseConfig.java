b34dff513edacb974f89205c7e4bdac440ae5d0e
JunRock <wnstjr120422@naver.com>
2024-03-22 00:46:46 +0900

    fix : 채팅수정

--- src/main/java/io/festival/distance/domain/firebase/config/FirebaseConfig.java
+++ src/main/java/io/festival/distance/domain/firebase/config/FirebaseConfig.java
@@ -1,4 +1,5 @@
 package io.festival.distance.domain.firebase.config;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import javax.annotation.PostConstruct;
@@ -26,8 +27,10 @@ public class FirebaseConfig {
 		System.out.println("myJson: " + myJson);
 		try {
 			System.out.println("절대경로>>> "+ System.getProperty("user.dir"));
+			System.out.println(new File("").getAbsolutePath());
+			System.out.println(String.format("%s/%s", new File("").getAbsolutePath(), "firebaseAdminSDKPrivateKey.json"));
 			//String fullPath = System.getProperty("user.dir") + "/" + FILEPATH;
-			String fullPath="../../../../../../firebaseAdminSDKPrivateKey.json";
+			String fullPath=String.format("%s/%s", new File("").getAbsolutePath(), "firebaseAdminSDKPrivateKey.json");
 			System.out.println(fullPath);
 			// FirebaseOptions 객체를 생성 (Firebase 접근시 서버 인증 정보)
 			FirebaseOptions firebaseOptions = FirebaseOptions.builder()
