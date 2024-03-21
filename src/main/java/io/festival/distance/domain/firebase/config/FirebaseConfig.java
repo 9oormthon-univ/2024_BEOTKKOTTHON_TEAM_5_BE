a9f2e905c61725e18ee607af56ec6741660c23ea
JunRock <wnstjr120422@naver.com>
2024-03-21 22:30:12 +0900

    fix : 경로 수정

--- src/main/java/io/festival/distance/domain/firebase/config/FirebaseConfig.java
+++ src/main/java/io/festival/distance/domain/firebase/config/FirebaseConfig.java
@@ -27,7 +27,7 @@ public class FirebaseConfig {
 		try {
 			System.out.println("절대경로>>> "+ System.getProperty("user.dir"));
 			//String fullPath = System.getProperty("user.dir") + "/" + FILEPATH;
-			String fullPath="/home/ubuntu/2024_BEOTKKOTTHON_TEAM_5_BE/firebaseAdminSDKPrivateKey.json";
+			String fullPath="/2024_BEOTKKOTTHON_TEAM_5_BE/firebaseAdminSDKPrivateKey.json";
 			System.out.println(fullPath);
 			// FirebaseOptions 객체를 생성 (Firebase 접근시 서버 인증 정보)
 			FirebaseOptions firebaseOptions = FirebaseOptions.builder()
